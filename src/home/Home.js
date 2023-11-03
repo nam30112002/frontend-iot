4f47d89f71d08cc6b0c0cc2b316899f6cd06d90a
nam30112002 <nam30112002>
2023-11-03 17:28:31 +0700

    dang ki, dang nhap, log out, xem thong tin ca nhan, danh sach nguoi dung

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,9 +1,187 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Button from '@mui/material/Button';
+import Cookies from 'js-cookie';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      showList: false,
+      listUser: [],
+      isAdmin: false,
+      reset: 0,
+    };
+  }
+
+  componentDidMount(){
+    this.checkAdmin();
+  }
+
+  hienThiDanhSach = () => {
+    console.log(typeof this.state.user)
+
+    let accessToken = Cookies.get('accessToken');
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Bearer ' + accessToken);
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("/users/me", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        try {
+          let jsonResult = JSON.parse(result);
+          this.state.user = jsonResult;
+          this.setState({
+            showList: true // Chỉ cập nhật thuộc tính showList, giữ nguyên user
+          });
+          console.log(this.state.user);
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+        }
+      })
+      .then(() => { console.log(this.state.showList) })
+      .catch(error => console.log('error', error));
+  }
+
+  logOut = () => {
+    Cookies.remove('accessToken');
+    window.location.href = "/login";
+  }
+
+  checkAdmin = () => {
+    let accessToken = Cookies.get('accessToken');
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Bearer ' + accessToken);
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch('/users/', requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log(1);
+          this.setState({
+            isAdmin: true,
+          })
+          console.log("nguoi dung la admin")
+          return response.json();
+        } else {
+          console.log("nguoi dung ko la admin")
+          this.setState({
+            isAdmin: false
+          })
+          throw new Error('Lỗi khi kiểm tra quyền admin');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  layDanhSachNguoiDung = () => {
+    let accessToken = Cookies.get('accessToken');
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Bearer ' + accessToken);
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("/users/", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        let jsonResult = JSON.parse(result);
+        console.log(typeof jsonResult)
+        console.log(typeof this.state.listUser)
+        console.log(jsonResult)
+        this.state.listUser = jsonResult;
+        this.setState({
+          reset: this.state.reset + 1,
+        })
+      })
+      .then(() => console.log(this.state.listUser))
+      .catch(error => console.log('error', error));
+  }
+
   render() {
+    let listDataUser;
+    let buttonGetAll;
+    let danhSachNguoiDung;
+    if (this.state.showList) {
+      listDataUser = <List>
+        <ListItem disablePadding>
+          <ListItemText>username: {this.state.user.username}</ListItemText>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemText>create at: {this.state.user.created_at}</ListItemText>
+        </ListItem>
+      </List>
+    }
+    if (this.state.isAdmin) {
+      buttonGetAll = <Button variant="contained" onClick={this.layDanhSachNguoiDung}>
+        Lay danh sach nguoi dung
+      </Button>;
+      danhSachNguoiDung = <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>username</TableCell>
+              <TableCell>id</TableCell>
+              <TableCell>Active?</TableCell>
+              <TableCell>Admin?</TableCell>
+              <TableCell>Created At</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.listUser.map((user) => (
+              <TableRow
+                key={user.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.is_active.toString()}</TableCell>
+                <TableCell>{user.is_superuser.toString()}</TableCell>
+                <TableCell>{user.created_at}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    }
+
     return (
-      <div>Home</div>
+      <>
+        <div>Home</div>
+        <Button variant="contained" onClick={this.hienThiDanhSach}>
+          Thong tin ca nhan
+        </Button>
+        <>{listDataUser}</>
+        <Button variant="contained" onClick={this.logOut}>
+          Log out
+        </Button>
+        <>{buttonGetAll}</>
+        <>{danhSachNguoiDung}</>
+      </>
     )
   }
 }
--- src/login/SignIn.js
+++ src/login/SignIn.js
@@ -8,23 +8,56 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-//import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-//import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function SignIn() {
-  const navigate = useNavigate ();
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    console.log(event.currentTarget);
+    console.log(data);
     console.log({
-      email: data.get('email'),
+      email: data.get('username'),
       password: data.get('password'),
     });
-    navigate('/home');
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("username", data.get('username'));
+    urlencoded.append("password", data.get('password'));
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    let dataAfterLogin = '';
+
+    fetch("/login", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        let jsonResult = JSON.parse(result);
+        dataAfterLogin = jsonResult.access_token; // Lưu giá trị vào biến ngoài hàm
+        Cookies.set('accessToken', dataAfterLogin, { expires: 7 }); //Cookie sẽ hết hạn sau 7 ngày
+      })
+      .then(() => console.log(dataAfterLogin))
+      .then(() => console.log(typeof dataAfterLogin))
+      .then(() =>  {
+        if(typeof dataAfterLogin != 'undefined'){
+          navigate('/home');
+        }
+      })
+      .catch(error => console.log('error', error));
+    // navigate('/home');
   };
 
   const handleSignUp = (event) => {
@@ -33,65 +66,65 @@ export default function SignIn() {
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            {/* Đây là một comment trong JSX <LockOutlinedIcon /> */}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="#" variant="body2" onClick = {handleSignUp}>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          {/* Đây là một coemailmment trong JSX <LockOutlinedIcon /> */}
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="#" variant="body2" onClick={handleSignUp}>
+                {"Don't have an account? Sign Up"}
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-      </Container>
-    
+      </Box>
+    </Container>
+
   );
 }
